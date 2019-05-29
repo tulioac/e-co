4a1f07dc73b1d7f03eae7e9a79bc65dfcd351dda
tutex1500 <tutex1500@gmail.com>
2019-05-29 11:48:55 -0300

    Implementação da US1

--- src/facade/EcoFacade.java
+++ src/facade/EcoFacade.java
@@ -1,14 +1,38 @@
 package facade;
 
+import easyaccept.EasyAccept;
+
 public class EcoFacade {
 
 	private PessoaController pessoaController;
+	
+	public EcoFacade() {
+		this.pessoaController = new PessoaController();
+	}
 
-	public void cadastraPessoa(String nome, String dni, String estado, String interesses) {
-		this.pessoaController.cadastraPessoa(nome, dni, estado, interesses);
+	public void limparSistema() {
+		// Persistencia de dados
 	}
 	
-	public void cadastraPessoa(String nome, String dni, String estado, String interesses, String partido) {
-		this.pessoaController.cadastraPessoa(nome, dni, estado, interesses, partido);
+	public void salvarSistema() {
+		// Persistencia de dados
+	}
+	
+	public void carregarSistema() {
+		// Persistencia de dados
+	}
+	
+	public void cadastrarPessoa(String nome, String dni, String estado, String interesses) {
+		this.pessoaController.cadastrarPessoa(nome, dni, estado, interesses);
+	}
+	
+	public void cadastrarPessoa(String nome, String dni, String estado, String interesses, String partido) {
+		this.pessoaController.cadastrarPessoa(nome, dni, estado, interesses, partido);
+	}
+	
+	public static void main(String[] args) {
+		args = new String[] { "facade.EcoFacade", "acceptance_tests/use_case_1.txt"};
+
+		EasyAccept.main(args);
 	}
 }
--- src/facade/PessoaController.java
+++ src/facade/PessoaController.java
@@ -30,11 +30,11 @@ public class PessoaController {
 			throw new IllegalArgumentException(mensagem);
 	}
 
-	public void cadastraPessoa(String nome, String dni, String estado, String interesses, String partido) {
-		validaString(nome, "Erro ao cadastrar pessoa: nome nao pode ser vazio ou nulo");
-		validaString(dni, "Erro ao cadastrar pessoa: dni nao pode ser vazio ou nulo");
-		validaDni(dni, "Erro ao cadastrar pessoa: dni invalido");
-		validaString(estado, "Erro ao cadastrar pessoa: estado nao pode ser vazio ou nulo");
+	public void cadastrarPessoa(String nome, String dni, String estado, String interesses, String partido) {
+		this.validaString(nome, "Erro ao cadastrar pessoa: nome nao pode ser vazio ou nulo");
+		this.validaString(dni, "Erro ao cadastrar pessoa: dni nao pode ser vazio ou nulo");
+		this.validaString(estado, "Erro ao cadastrar pessoa: estado nao pode ser vazio ou nulo");
+		this.validaDni(dni, "Erro ao cadastrar pessoa: dni invalido");
 		
 		if (this.pessoas.containsKey(dni))
 			throw new IllegalArgumentException("Erro ao cadastrar pessoa: dni ja cadastrado");
@@ -42,7 +42,7 @@ public class PessoaController {
 		this.pessoas.put(dni, new Pessoa(nome, dni, estado, interesses, partido));
 	}
 
-	public void cadastraPessoa(String nome, String dni, String estado, String interesses) {
-		this.cadastraPessoa(nome, dni, estado, interesses, null); 
+	public void cadastrarPessoa(String nome, String dni, String estado, String interesses) {
+		this.cadastrarPessoa(nome, dni, estado, interesses, null); 
 	}
 }
