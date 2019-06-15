6cdaa62755789fcb4087ed5ccbac5699ed6192f4
jonathantvrs <jonathan.tavares.silva@ccc.ufcg.edu.br>
2019-06-15 15:15:36 -0300

    atualizando nome de teste de unidade para PartidoBaseControllerTest

--- testes/controllers/PartidoBaseControllerTest.java
+++ testes/controllers/PartidoBaseControllerTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class PartidoControllerTest {
+class PartidoBaseControllerTest {
 
     private PartidoBaseController pc1;
 
