f9ad6f3418f0f0cba3679c9681e30e291ba1274b
MirellaLyra <mirella.lyra@ccc.ufcg.edu.br>
2019-06-16 16:40:41 -0300

    implementacao da classe de teste de PL

--- testes/entities/PLPTest.java
+++ testes/entities/PLPTest.java
@@ -13,9 +13,10 @@ class PLPTest {
 		this.plp = new PLP("10/2019","111111111-1", 2019, "Reduz a tributacao em cima de bicicletas", "meio-ambiente, transportes, esportes", "https://example.net/bicicleta%40tributos", "123, 12");
 
 	}
+	
 	@Test
 	void testaConstrutor() {
-		new PLP("1/2005","111111000-1", 2005, "Destina 30% das muultas de trânsito arrecadadas à melhoria da acessibilidade urbana",
+		new PLP("1/2005","111111000-1", 2005, "Destina 30% das multas de trânsito arrecadadas à melhoria da acessibilidade urbana",
 				"inclusao, transportes", "https://example.net/multas%22acessibilidade", "36, 70");
 		new PLP("1/2019","991111000-1", 2019, "Proibe a venda de produtos com cafeina a menores",
 				"juventude", "https://example.net/cafeina%23menores", "23, 75");
@@ -23,9 +24,10 @@ class PLPTest {
 
 	@Test
 	void testaToString() {
-		assertEquals("Projeto de Lei Complementar - PLP 10/2019 - " +
+		assertEquals("Projeto de Lei Complementar - 10/2019 - " +
                 "111111111-1 - Reduz a tributacao em cima de bicicletas - " +
-                "123, 12 - EM VOTAÇÃO (CCJC)", this.plp.toString());
+                "123,  12 - EM VOTACAO (CCJC)",
+        this.plp.toString());
 	}
 	
 }
