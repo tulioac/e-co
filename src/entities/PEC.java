a294823268c79039a943da4f253054c176826a4e
jonathantvrs <jonathan.tavares.silva@ccc.ufcg.edu.br>
2019-06-18 03:51:13 -0300

    alteração no método toString de Pec e organizando documentação

--- src/entities/PEC.java
+++ src/entities/PEC.java
@@ -19,7 +19,7 @@ public class PEC extends Projeto implements Serializable {
     private String artigos;
 
     /**
-     * Constrói um Projeto do tipo PEC inicializando o tipo de projeto como PEC.
+     * Constrói um projeto de emenda constitucional inicializando o tipo de projeto como PEC.
      */
     public PEC(String codigo, String dniAutor, int ano, String ementa, String interesses, String endereco, String artigos) {
         super(codigo, dniAutor, ano, ementa, interesses, endereco);
@@ -29,15 +29,14 @@ public class PEC extends Projeto implements Serializable {
 
     /**
      * Retorna uma representaçao em String da PEC sobreescrevendo o método toString que foi criado na
-     * classe Projeto exibindo os artigos que foram referenciados nessa PEC.
+     * classe Projeto e exibindo os artigos que foram referenciados nessa PEC.
      *
      * @return string no formato Projeto de Emenda Constitucional - codigo - dni do autor do projeto - ementa - artigos - situacao.
      */
     @Override
     public String toString() {
-        StringBuilder representacaoDeProjeto = new StringBuilder("Projeto de Emenda Constitucional - " + super.toString() + " - " + this.getArtigos() + " - " + this.exibeSituacaoAtual());
-
-        return representacaoDeProjeto.toString();
+        return "Projeto de Emenda Constitucional - " + super.toString()
+                + " - " + this.getArtigos() + " - " + this.exibeSituacaoAtual();
     }
 
     /**
