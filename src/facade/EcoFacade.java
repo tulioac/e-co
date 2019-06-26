191387e4b0088f8250dbac1c937507f7348321fb
Guilherme Carneiro <guilherme.carneiro@ccc.ufcg.edu.br>
2019-06-26 16:12:30 -0300

    documenta us9 ecoFacade

--- src/facade/EcoFacade.java
+++ src/facade/EcoFacade.java
@@ -280,18 +280,31 @@ public class EcoFacade {
     }
     
     /**
+     * Retorna o código da proposta mais relacionada a pessoa do dni, com base
+     * no número de interesses em comum com a proposta. Caso não haja interesses
+     * em comum, uma String vazia é retornada. Caso haja mais de uma proposta com
+     * o mesmo número de interesses em comum, vale a estratégia de desempate definida
+     * pelo usuário no sistema. Os últimos critérios de desempate são: ano de cadastro
+     * da proposta e código da proposta, por último, se o ano também for igual.Lança
+     * IllegalArgumentException e NullPointerException para entradas inválidas ou nulas,
+     * respectivamente.
      * 
-     * @param dni
-     * @return
+     * @param dni dni da pessoa a qual se quer buscar a proposta mais relacionada
+     * @return código da proposta mais relacionada, String vazia se não houver nenhuma
      */
     public String pegarPropostaRelacionada(String dni) {
     	return this.projetoController.getPropostaRelacionada(dni);
     }
     
     /**
+     * Redefine a estratégia de desempate da busca por uma proposta mais relacionada, com
+     * base no dni da pessoa e na estratégia nova a ser sobreposta a antiga. Essa estratégia
+     * só pode ser dos tipos: "APROVACAO", "CONCLUSAO" e "CONSTITUCIONAL". Lança
+     * IllegalArgumentException e NullPointerException para entradas inválidas ou nulas,
+     * respectivamente.
      * 
-     * @param dni
-     * @param estrategia
+     * @param dni dni da pessoa que se quer modificar a estratégia
+     * @param estrategia nova estratégia a ser redefinida
      */
     public void configurarEstrategiaPropostaRelacionada(String dni, String estrategia) {
     	this.projetoController.configurarEstrategiaPropostaRelacionada(dni, estrategia);
