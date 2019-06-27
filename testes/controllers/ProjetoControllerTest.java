b1796fb4d07cff816bd34fdc3511c6d1b23cdd07
tutex1500 <tutex1500@gmail.com>
2019-06-26 21:46:30 -0300

    Votaçao de PLs

--- testes/controllers/ProjetoControllerTest.java
+++ testes/controllers/ProjetoControllerTest.java
@@ -6,8 +6,7 @@ import services.ComissaoService;
 import services.PartidoBaseService;
 import services.PessoaService;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ProjetoControllerTest {
     private ProjetoController pc;
@@ -27,6 +26,28 @@ class ProjetoControllerTest {
         this.pbc = new PartidoBaseController();
         this.pas = new PartidoBaseService(pbc);
         this.pc = new ProjetoController(ps, cs, pas);
+    }
+
+    @BeforeEach
+    void configuraParaVotacao() {
+        this.p.cadastrarPessoa("pedrinho", "111111111-1", "PB", "educacao,seguranca publica,saude", "PartidoGov");
+        this.p.cadastrarPessoa("gilberto", "222222222-2", "PE", "educacao,seguranca publica,saude", "PartidoGov");
+        this.p.cadastrarPessoa("alfredo", "333333333-3", "PI", "saude,seguranca publica,trabalho", "PartidoGov");
+        this.p.cadastrarPessoa("jarbas", "444444444-4", "PI", "saude,seguranca publica,trabalho", "PartidoOpo");
+        this.p.cadastrarPessoa("jorete", "555555555-5", "PI", "nutricao", "PartidoOpo");
+
+        this.p.cadastrarDeputado("111111111-1", "24012013");
+        this.p.cadastrarDeputado("222222222-2", "24012013");
+        this.p.cadastrarDeputado("333333333-3", "24012013");
+        this.p.cadastrarDeputado("444444444-4", "24012013");
+        this.p.cadastrarDeputado("555555555-5", "24012013");
+
+        this.pbc.cadastrarPartido("PartidoGov");
+
+        this.cc.cadastrarComissao("CCJC", "111111111-1,222222222-2,333333333-3");
+        this.cc.cadastrarComissao("CTF", "111111111-1,222222222-2,444444444-4,555555555-5");
+        this.cc.cadastrarComissao("CGOV", "111111111-1,333333333-3,444444444-4,555555555-5");
+
     }
 
     @Test
@@ -142,5 +163,50 @@ class ProjetoControllerTest {
         this.p.cadastrarPessoa("Pedrao", "312534654-5", "PB", "saude", "POT");
 
         assertThrows(IllegalArgumentException.class, () -> this.pc.cadastraPEC("312534654-5", 2013, "Ementa PL Conclusiva", "saude, educacao", "wwww.ementa.com.br", "4,5"));
+    }
+
+    @Test
+    void testaVotarProjetoInexistente() {
+        assertThrows(NullPointerException.class, () -> this.pc.votarComissao("PP 1/2018", "OPOSICAO", "CGOV"));
+    }
+
+    @Test
+    void testaVotarProjetoComStatusInvalido() {
+        assertThrows(IllegalArgumentException.class, () -> this.pc.votarComissao("PP 1/2018", "A FAVOR", "CGOV"));
+    }
+
+    @Test
+    void testaVotarProjetoComLocalVazio() {
+        assertThrows(IllegalArgumentException.class, () -> this.pc.votarComissao("PP 1/2018", "A FAVOR", ""));
+    }
+
+    @Test
+    void testaVotarProjetoNoPlenarioDaComissao() {
+        this.pc.cadastraPL("111111111-1", 2013, "Ementa PL Conclusiva", "saude, educacao", "wwww.ementa.com.br", true);
+        assertThrows(IllegalArgumentException.class, () -> this.pc.votarPlenario("PL 1/2013", "OPOSICAO", "111111111-1,333333333-3,555555555-5"));
+    }
+
+    @Test
+    void testaVotarPLComissaoConclusiva() {
+
+        this.pc.cadastraPL("111111111-1", 2013, "Ementa PL Conclusiva", "saude, educacao", "wwww.ementa.com.br", true);
+
+        assertTrue(this.pc.votarComissao("PL 1/2013", "GOVERNISTA", "CTF"));
+        assertFalse(this.pc.votarComissao("PL 1/2013", "GOVERNISTA", "CGOV"));
+
+        assertThrows(IllegalArgumentException.class, () -> this.pc.votarComissao("PL 1/2013", "OPOSICAO", "-"));
+    }
+
+    @Test
+    void testaVotarPLComissaoNaoConclusiva() {
+
+        this.pc.cadastraPL("111111111-1", 2013, "Ementa PL Conclusiva", "saude, educacao", "wwww.ementa.com.br", false);
+
+        assertTrue(this.pc.votarComissao("PL 1/2013", "GOVERNISTA", "CTF"));
+        assertFalse(this.pc.votarComissao("PL 1/2013", "GOVERNISTA", "plenario"));
+
+        assertTrue(this.pc.votarPlenario("PL 1/2013", "OPOSICAO", "111111111-1,444444444-4,555555555-5"));
+
+        assertEquals("POL: pedrinho - 111111111-1 (PB) - PartidoGov - Interesses: educacao,seguranca publica,saude - 24/01/2013 - 1 Leis", this.p.exibirPessoa("111111111-1"));
     }
 }
