705c9d366fae7dd51f1cdbbda803803401c95a42
jonathantvrs <jonathan.tavares.silva@ccc.ufcg.edu.br>
2019-06-28 09:45:29 -0300

    retira ponto e virgula desnecessario em enum StatusGovernista

--- src/enums/StatusGovernista.java
+++ src/enums/StatusGovernista.java
@@ -6,5 +6,5 @@ import java.io.Serializable;
  * Enumera todos os possíveis status de um projeto
  */
 public enum StatusGovernista implements Serializable {
-    GOVERNISTA, OPOSICAO, LIVRE;
+    GOVERNISTA, OPOSICAO, LIVRE
 }
