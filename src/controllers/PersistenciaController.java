58ef333d55132a3b8711b89562399fe2a52e095b
jonathantvrs <jonathan.tavares.silva@ccc.ufcg.edu.br>
2019-06-28 17:48:20 -0300

    implementação dos metodos de salvar

--- src/controllers/PersistenciaController.java
+++ src/controllers/PersistenciaController.java
@@ -1,8 +1,15 @@
 package controllers;
 
+import entities.Comissao;
+import entities.Partido;
+import entities.Pessoa;
+import interfaces.PropostaLegislativa;
 import services.ProjetoService;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class PersistenciaController {
@@ -19,22 +26,7 @@ public class PersistenciaController {
     }
 
     public void salvarSistema() {
-        ObjectOutputStream objGravador = null;
-        try {
-            objGravador = new ObjectOutputStream(new FileOutputStream("dados/eco.txt"));
 
-            objGravador.writeObject(new Object());
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        } finally {
-            if (objGravador != null) {
-                try {
-                    objGravador.close();
-                } catch (IOException ioe) {
-                    ioe.printStackTrace();
-                }
-            }
-        }
     }
 
     public void carregarSistema() {
@@ -49,6 +41,82 @@ public class PersistenciaController {
             if (objLeitor != null) {
                 try {
                     objLeitor.close();
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void salvarComissoes() {
+        Set<Comissao> comissoes = new HashSet<>(this.projetoService.getComissaoService().getComissoes());
+        ObjectOutputStream objGravador = null;
+        try {
+            objGravador = new ObjectOutputStream(new FileOutputStream("dados/comissoes.txt"));
+            objGravador.writeObject(comissoes);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } finally {
+            if (objGravador != null) {
+                try {
+                    objGravador.close();
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void salvarPessoas() {
+        Set<Pessoa> pessoas = new HashSet<>(this.projetoService.getPessoaService().getPessoas());
+        ObjectOutputStream objGravador = null;
+        try {
+            objGravador = new ObjectOutputStream(new FileOutputStream("dados/pessoas.txt"));
+            objGravador.writeObject(pessoas);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } finally {
+            if (objGravador != null) {
+                try {
+                    objGravador.close();
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void salvarPartidos() {
+        Set<Partido> partidos = new HashSet<>(this.projetoService.getPartidoService().getPartidos());
+        ObjectOutputStream objGravador = null;
+        try {
+            objGravador = new ObjectOutputStream(new FileOutputStream("dados/partidos.txt"));
+            objGravador.writeObject(partidos);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } finally {
+            if (objGravador != null) {
+                try {
+                    objGravador.close();
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void salvarPropostas() {
+        Set<PropostaLegislativa> propostas = new HashSet<>(this.projetoService.getPropostas());
+        ObjectOutputStream objGravador = null;
+        try {
+            objGravador = new ObjectOutputStream(new FileOutputStream("dados/propostas.txt"));
+            objGravador.writeObject(propostas);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } finally {
+            if (objGravador != null) {
+                try {
+                    objGravador.close();
                 } catch (IOException ioe) {
                     ioe.printStackTrace();
                 }
