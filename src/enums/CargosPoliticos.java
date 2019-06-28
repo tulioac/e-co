0d93829204ed516e819f981d17f6fc577f642920
jonathantvrs <jonathan.tavares.silva@ccc.ufcg.edu.br>
2019-06-28 09:52:04 -0300

    corrige ultimo enum CargosPoliticos

--- src/enums/CargosPoliticos.java
+++ src/enums/CargosPoliticos.java
@@ -4,6 +4,11 @@ import java.io.Serializable;
 
 /**
  * Enumera Cargos Políticos que uma Pessoa pode assumir
+ *
+ * @author Jonathan Tavares da Silva
+ * @author Mirella Quintans Lyra
+ * @author Tulio Araujo Cunha
+ * @author Guilherme de Melo Carneiro
  */
 public enum CargosPoliticos implements Serializable {
     DEPUTADO, SEM_CARGO
