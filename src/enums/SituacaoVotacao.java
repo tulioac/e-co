53b0423a27fcc75dde8ea69f47a06a2801dcec0d
jonathantvrs <jonathan.tavares.silva@ccc.ufcg.edu.br>
2019-06-28 09:46:47 -0300

    retira ponto e virgula desnecessario em enum SituacoesVotacao e melhora documentação

--- src/enums/SituacaoVotacao.java
+++ src/enums/SituacaoVotacao.java
@@ -3,8 +3,8 @@ package enums;
 import java.io.Serializable;
 
 /**
- * Enumera todas as situações de votação de um projeto.
+ * Enumera todas as possíveis situações de votação de um projeto.
  */
 public enum SituacaoVotacao implements Serializable {
-    EM_VOTACAO, REJEITADO, APROVADO;
+    EM_VOTACAO, REJEITADO, APROVADO
 }
